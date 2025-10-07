50e2cd70ef9386263216e94e8507942b8d177f9c
Faith Kalamshe <magagulachari@gmail.com>
2025-10-08 00:16:39 +0200

    React and Tailwind setup for Recipe Finder project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center p-8">
+      <h1 className="text-3xl font-bold text-blue-500">Recipe Finder</h1>
+      <p className="text-gray-600 mt-2">welcome to Recipe Finder </p>
+    </div>
+  )
+}
+
+export default App
+
