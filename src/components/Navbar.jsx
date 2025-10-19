76dbf24341f869f5c3104c5c00d00b493ac5e105
Faith Kalamshe <magagulachari@gmail.com>
2025-10-19 11:22:29 +0200

    Fixed route paths for Hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
+
 
 const Navbar = () => {
   const location = useLocation();
+  const [isOpen, setIsOpen] = useState(false);
 
   const linkClasses = (path) =>
     `px-4 py-2 rounded-lg font-semibold transition duration-300 ${
@@ -16,7 +19,35 @@ const Navbar = () => {
          Recipe Finder
       </Link>
 
-<div className="flex gap-4">
+  {/*mobile menu button*/}
+<button
+  onClick={() => setIsOpen(!isOpen)}
+  className="md:hidden text-orange-700 focus:outline-none"
+>
+  {isOpen ? (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+    </svg>
+  ) : (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+    </svg>
+  )}
+</button>
+
+<div className="hidden md:flex gap-4">
   <Link to="/home" className={linkClasses("/home")}>
     Home
   </Link>
@@ -30,6 +61,15 @@ const Navbar = () => {
     Desserts
   </Link>
 </div>
+{/*dropdown*/}
+{isOpen && (
+  <div className="flex flex-col items-start space-y-2 mt-3 md:hidden animate-slide-down">
+    <Link to="/home" onClick={() => setIsOpen(false)} className={linkClasses("/home")}>Home</Link>
+    <Link to="/category/starters" onClick={() => setIsOpen(false)} className={linkClasses("/category/starters")}>Starters</Link>
+    <Link to="/category/maincourse" onClick={() => setIsOpen(false)} className={linkClasses("/maincourse")}>Main Course</Link>
+    <Link to="/category/desserts" onClick={() => setIsOpen(false)} className={linkClasses("/desserts")}>Desserts</Link>
+  </div>
+)}
     </nav>
   );
 };
